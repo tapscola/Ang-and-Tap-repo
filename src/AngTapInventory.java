9d103c0eec05681ac3eaf4d61242e27a5d62d124
Tapiwa Musasa <tapiwa.musasa@blackmores.com.au>
2018-02-17 22:50:56 +1100

    Initial files

---
+++ src/AngTapInventory.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+/**
+ * @author Tapiwa Musasa && Ang Li
+ *
+ */
+public abstract class AngTapInventory {
+
+	/**
+	 * 
+	 */
+	public AngTapInventory() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
